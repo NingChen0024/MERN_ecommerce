efd173f08f5e618e5b95ec24ab233a668c4700ec
Ning <nche0024@gmail.com>
2021-01-16 09:54:04 +1100

    shopping cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom'
+import MessageBox from '../components/MessageBox';
 import { addToCart, changeQty, deleteItem } from '../redux/cart/cartActions';
 
 function CartScreen(props) {
@@ -28,61 +30,74 @@ function CartScreen(props) {
   return (
     <div>
       <div>Shopping Cart</div>
-      <div className='row'>
-        <div>
-          {
-            cartItems.map((item, ) => {
-              return (
-                  <div className='row top' key={item.product}>
-                    <div className='item-row'>
-                      <img className='small' src={item.image} alt={item.name} />  
+      {
+        cartItems.length === 0 ? (
+          <MessageBox>
+            Shopping Cart is Empty
+            <Link to='/'>
+              <button className='alert'>
+                Let's go shopping!
+              </button>
+            </Link>
+          </MessageBox>
+        ):(
+          <div className='row'>
+            <div>
+              {
+                  cartItems.map((item, ) => {
+                    return (
+                        <div className='row top' key={item.product}>
+                          <div className='item-row'>
+                            <img className='small' src={item.image} alt={item.name} />  
+                          </div>
+                          <div className='item-row text'>
+                            <div>{item.name}</div>
+                          </div>
+                          <div className='item-row'>
+                            <select value={item.qty} onChange={(e) => qtyChangeHandler(item.product,e.target.value)}>
+                              {            
+                                [...Array(item.countInStock).keys()].map(x => (
+                                  <option key={ x + 1} value={ x + 1 }>{ x + 1 }</option>
+                                ))
+                              }
+                            </select>
+                          </div>
+                          <div className='item-row text'>
+                            <div>${item.price}</div>
+                          </div>                 
+                          <div className='item-row'>
+                            <button
+                              onClick={() => deleteItemHandler(item.product)}
+                            >
+                              Delete
+                            </button>
+                          </div>
+                        </div>
+                    )
+                  })
+              }
+            </div>
+            <div>
+              <div className='card card-body'>
+                <ul>
+                  <li>
+                    <div className='row'>
+                      {`Subtotal(${cartItems.length} items):${
+                        cartItems.reduce((total, item) => total + item.qty * item.price, 0)
+                      }`}
                     </div>
-                    <div className='item-row'>
-                      <div>{item.name}</div>
+                  </li>
+                  <li>
+                    <div className='row'>
+                      <button className='primary'>Proceed to Checkout</button>
                     </div>
-                    <div className='item-row'>
-                      <select value={item.qty} onChange={(e) => qtyChangeHandler(item.product,e.target.value)}>
-                        {            
-                          [...Array(item.countInStock).keys()].map(x => (
-                            <option key={ x + 1} value={ x + 1 }>{ x + 1 }</option>
-                          ))
-                        }
-                      </select>
-                    </div>
-                    <div className='item-row'>
-                      <div>${item.price}</div>
-                    </div>                 
-                    <div className='item-row'>
-                      <button
-                        onClick={() => deleteItemHandler(item.product)}
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </div>
-              )
-            })
-          }
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-        <div>
-          <div className='card card-body'>
-            <ul>
-              <li>
-                <div className='row'>
-                  {`Subtotal(${cartItems.length} items):${
-                    cartItems.reduce((total, item) => total + item.qty * item.price, 0)
-                  }`}
-                </div>
-              </li>
-              <li>
-                <div className='row'>
-                  <button className='primary'>Proceed to Checkout</button>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+        )
+      }
     </div>
   )
 }
